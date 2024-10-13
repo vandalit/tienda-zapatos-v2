32d52664ace72d8a8c7e854cb0a4e3cb7ab551cd
Van <84937597+vandalit@users.noreply.github.com>
2024-10-12 23:46:10 -0300

    aleluya! firebase deployed

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import firebaseApp from './firebaseConfig';
+// import firebaseApp from './firebaseConfig';
 
 
 const app = createApp(App)
